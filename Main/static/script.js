5dee603bb4b18a773c5323b11dec252a7b996e49
sadra <sadrataghavi1383@gmail.com>
2021-06-12 18:12:48 +0430

    remove css and js files from ignore for this project

---
+++ Main/static/script.js
@@ -0,0 +1,11 @@
+($(document).ready(function(){
+    image_click()
+}))
+
+
+
+function image_click(){
+    $(".image").click(function(){
+        window.open("https://github.com/SadraTghvi")
+    })
+}
--- Main/views.py
+++ Main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
