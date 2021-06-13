1b5cf864ba9f1a3d2c5680a32769baeb8270417d
sadra <sadrataghavi1383@gmail.com>
2021-06-13 22:40:24 +0430

    functionality for handling them

--- Main/views.py
+++ Main/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.core.mail import EmailMessage
+from django.contrib import messages
 
 # Create your views here.
 
 def main(request):
     return render(request,"base.html")
+
+def send_email(request):
+    stuff = request.POST
+    print(stuff)
+    return redirect("main")
