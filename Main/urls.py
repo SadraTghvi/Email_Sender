dda53048a4f14e1af4b462e12602c8776ce59e7b
sadra <sadrataghavi1383@gmail.com>
2021-06-13 22:40:05 +0430

    url for taking stuff from front-end and passing them to backend

--- Main/urls.py
+++ Main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path("",main,name="main")
+    path("",main,name="main"),
+    path("send_email/", send_email, name="send_email"),
 ]
 
