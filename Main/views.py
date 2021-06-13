ae8f2ed878cd49b40041d7e9460d06b7ff4ff3e8
sadra <sadrataghavi1383@gmail.com>
2021-06-13 23:11:07 +0430

    added messages from django.contrib for api

--- Main/views.py
+++ Main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
+from django.http import HttpResponse
 from django.core.mail import EmailMessage
 from django.contrib import messages
+import time
 
 # Create your views here.
 
@@ -8,6 +10,12 @@ def main(request):
     return render(request,"base.html")
 
 def send_email(request):
-    stuff = request.POST
-    print(stuff)
-    return redirect("main")
+    email = request.POST["email"]
+    subject = request.POST["subject"]
+    name = request.user.username
+    if subject and email:
+        messages.info(request,"succes")
+        return render(request, "form.html")
+    else:
+        messages.info(request, "nop")
+        return redirect("main")
