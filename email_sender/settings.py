a4573ad42333256f7d5ec5041b1050925809fe04
sadra <sadrataghavi1383@gmail.com>
2021-06-14 00:07:21 +0430

    added gmail fields for sending gmail

--- email_sender/settings.py
+++ email_sender/settings.py
@@ -130,3 +130,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sadravalorant@gmail.com'
+EMAIL_HOST_PASSWORD = 'Sadravalorant3'
+EMAIL_PORT = 587
