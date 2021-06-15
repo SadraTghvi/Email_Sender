9515ab823ba8ef5468f0b225970427f93057b88a
sadra <sadrataghavi1383@gmail.com>
2021-06-15 23:27:46 +0430

    renaming app to Account and addin url in site

--- account/apps.py
+++ account/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class accountConfig(AppConfig):
+class AccountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'account'
+    name = 'Account'
