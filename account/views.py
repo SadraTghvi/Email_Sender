f7a417ea28bf9989906a8f62514c52c57662d712
sadra <sadrataghavi1383@gmail.com>
2021-06-16 00:30:54 +0430

    adding login view for user login form

--- account/views.py
+++ account/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.views import LoginView,LogoutView
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+# class AccountLogin(LoginView):
+
+
+# class AccountLogout(LogoutView):
+
+def main(request):
+    return render(request,"account_base.html")
+
+class AccountLogin(LoginView):
+    template_name = 'login.html'
+    redirect_authenticated_user = False
+    def get_success_url(self):
+        return reverse_lazy("main:main")
