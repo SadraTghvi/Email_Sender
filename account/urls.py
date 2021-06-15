f3edc031493eb4bd4ce9924abffd71159f8fc2d6
sadra <sadrataghavi1383@gmail.com>
2021-06-15 23:36:08 +0430

    adding urls for signin/login/...^C

--- account/urls.py
+++ account/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path,include
+
+
+urlpatterns = [
+    path("/Signin/",...,name=""),
+    path("/Signout/",...,name=""),
+    path("/Login/",...,name=""),
+    path("/Logout/",...,name=""),
+]
