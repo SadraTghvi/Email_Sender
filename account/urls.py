39db92bfdb235dd9afeb8af1955c1c853f6292db
sadra <sadrataghavi1383@gmail.com>
2021-06-16 01:13:25 +0430

    and urls for logout login ... added / also login.html updated with some styling

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,7 @@ from Account.views import *
 urlpatterns = [
     path("signin/",main,name="main"),
     path("login/",AccountLogin.as_view(),name="login"),
+    path("logout/",AccountLogout.as_view(),name="logout"),
 ]
 
 app_name = "account"
