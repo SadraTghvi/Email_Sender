e26ebf2a7552ca46b439cdbb1d5f8a4ccf080f8c
sadra <sadrataghavi1383@gmail.com>
2021-06-17 01:51:44 +0430

    login reqired added for unauthenticated users

--- Main/views.py
+++ Main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.core.mail import EmailMessage,send_mail
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 import time
 
