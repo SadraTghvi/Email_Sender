4cb4a3df22e85b171f040787a18a2d6dfa621bc2
sadra <sadrataghavi1383@gmail.com>
2021-06-17 01:53:33 +0430

    urls for login,logout,... added

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path,include
+from django.urls import path, include
 from Account.views import *
 
 
 urlpatterns = [
-    path("signin/",main,name="main"),
-    path("login/",AccountLogin.as_view(),name="login"),
-    path("logout/",AccountLogout.as_view(),name="logout"),
+    path("login/",  AccountLogin.as_view(), name="login"),
+    path("logout/", logout, name="logout"),
+    path("signup/", AccountSignup.as_view(), name="signup"),
 ]
 
 app_name = "account"
